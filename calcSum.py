39ac9b154bb27cf079398e5d2551c32eb64a605d
Bryce Henderson <brycehenderson257@ou.edu>
2023-09-12 16:19:34 -0500

    Added function to find product of list

--- calcSum.py
+++ calcSum.py
@@ -5,3 +5,12 @@ def calcSum(list):
     return sum
 
 print(calcSum([1, 2, 3, 4, 5]))
+
+
+def calcProduct(list):
+    product = 1
+    for i in list:
+        product = product * list[i-1]
+    return product
+
+print(calcProduct([1,2,3,4]))
